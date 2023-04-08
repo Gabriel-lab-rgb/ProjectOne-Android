92b35c713898596d959a92eba537a4c0adddd6ab
Gabriel-lab-rgb <grivote615@iesmartinezm.es>
2023-04-08 20:01:05 +0200

    Commit

---
+++ app/src/main/java/com/example/projectone/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.projectone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener {
+
+  private BottomNavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navigationView=findViewById(R.id.bottomNavigationView);
+        navigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.community:
+                HomeFragment home=new HomeFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,home).commit();
+                break;
+            case R.id.activity:
+                ActivityFragment activity=new ActivityFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,activity).commit();
+                break;
+            case R.id.profile:
+                ProfileFragment profile=new ProfileFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,profile).commit();
+                break;
+            case R.id.messages:
+                break;
+
+        }
+
+        return false;
+    }
+}
