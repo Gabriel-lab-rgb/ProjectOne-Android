796c03fa542cb36b1af05d7c90a0ec5ba4d49241
Gabriel-lab-rgb <grivote615@iesmartinezm.es>
2023-05-03 18:42:49 +0200

    Commit

--- app/src/main/java/com/example/projectone/MainActivity.java
+++ app/src/main/java/com/example/projectone/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.projectone;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -10,15 +11,23 @@ import android.view.View;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener {
 
   private BottomNavigationView navigationView;
+  private ArrayList<Fragment> fragmentArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /* fragmentArrayList.add(new HomeFragment());
+        fragmentArrayList.add(new ActivityFragment());
+        fragmentArrayList.add(new ProfileFragment());*/
+
         navigationView=findViewById(R.id.bottomNavigationView);
         navigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
 
@@ -30,15 +39,19 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
         switch (item.getItemId()){
             case R.id.community:
                 HomeFragment home=new HomeFragment();
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,home).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,home).addToBackStack(null).commit();
                 break;
             case R.id.activity:
                 ActivityFragment activity=new ActivityFragment();
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,activity).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,activity).addToBackStack(null).commit();
                 break;
             case R.id.profile:
                 ProfileFragment profile=new ProfileFragment();
-                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,profile).commit();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,profile).addToBackStack(null).commit();
+                break;
+            case R.id.add:
+                CreatePostFragment createPost=new CreatePostFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout,createPost).addToBackStack(null).commit();
                 break;
             case R.id.messages:
                 break;
